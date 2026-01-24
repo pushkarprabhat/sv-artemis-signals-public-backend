c30eb1e0737dbae71dabc2e5970bf92d6f1632ae
pushkarprabhat <pushkarprabhat@users.noreply.github.com>
2026-01-25 00:17:35 +0530

    feat(commit): set default author and committer information for commits

---
+++ commit_cb.py
@@ -0,0 +1,5 @@
+﻿def commit_callback(commit):
+    commit.author_name = b"pushkarprabhat"
+    commit.author_email = b"25482110+pushkarprabhat@users.noreply.github.com"
+    commit.committer_name = b"pushkarprabhat"
+    commit.committer_email = b"25482110+pushkarprabhat@users.noreply.github.com"
